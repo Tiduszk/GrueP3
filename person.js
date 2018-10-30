9afe5cb6cda5c1d3a88b13066479bb953e82a520
sustac <42981293+sustac@users.noreply.github.com>
2018-10-30 14:17:25 -0400

    newest

---
+++ person.js
@@ -0,0 +1,22 @@
+var myjavaclass = Java.type('script.personScript');
+
+function person(name, health, score){
+	this.name = name;
+	this.health = health;
+	this.score = score;
+	
+	this.getPerson = function(){
+		var ArrayList = Java.type('java.util.ArrayList');
+		var myPerson = new ArrayList();
+		myPerson.add(this.name);
+		myPerson.add(this.health);
+		myPerson.add(this.score);
+		return myPerson;
+		}
+}
+
+var person1 = new person("venom", "100", "0");
+myjavaclass.person(person1);
+
+var person2 = new person("spiderman", "65", "1000");
+myjavaclass.person(person2);
