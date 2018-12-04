49c2395d2c21add40a017ac9524650b4df544315
sustac <42981293+sustac@users.noreply.github.com>
2018-12-04 15:41:18 -0500

    new

--- engine/Item.java
+++ engine/Item.java
@@ -2,14 +2,20 @@
 
 package engine;
 
+import java.util.ArrayList;
+
 public class Item extends Interactable{
 	int uses;
+	String type;
+	ArrayList<String> item_unlocks = new ArrayList<String>();
 
 	public Item() {}
 	
-	public Item(String name , String description, int uses) {
+	public Item(String name , String description, String type, ArrayList<String> item_unlocks, int uses) {
 		this.name = name;
 		this.description = description;
+		this.type = type;
+		this.item_unlocks = item_unlocks;
 		this.uses = uses;
 	}
 	
